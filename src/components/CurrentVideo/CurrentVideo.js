ead020ba8cebf95b232bf1ce6b0aec2f82084077
BigBoiJonny <jonny98.0@outlook.com>
2023-03-07 16:51:09 +0000

    get axios data

--- src/components/CurrentVideo/CurrentVideo.js
+++ src/components/CurrentVideo/CurrentVideo.js
@@ -1,9 +1,9 @@
 import "./CurrentVideo.scss";
 
-const CurrentVideo = ({ currentvideo }) => {
+const CurrentVideo = (props) => {
   return (
     <div className="video__wrapper">
-      <video controls className="video__poster" poster={currentvideo.image}>
+      <video controls className="video__poster" poster={props.image}>
         Your browser does not support the video tag.
       </video>
     </div>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,52 +1,75 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import CurrentVideo from "../../components/CurrentVideo/CurrentVideo";
 import VideoDescription from "../../components/VideoDescription/VideoDescription";
-import videodetails from "../../assets/data/video-details.json";
+
 import AddComment from "../../components/AddComment/AddComment";
 import Comments from "../../components/Comments/Comments";
 import VideoList from "../../components/VideoList/VideoList";
-import videoSmallListData from "../../assets/data/videos.json";
 
 import "./Home.scss";
 
 const Home = () => {
+  const key = "4cc34eef-8f4a-4587-ad97-0594e067e347";
+
   const defaultText = "Add a new comment";
   const [text, setText] = useState(defaultText);
-
   const changeText = (newText) => {
     setText(newText);
   };
 
-  const [video, setVideo] = useState(videodetails[0]);
+  //   const [video, setVideo] = useState();
 
   // list of all video details
-  const [VideoDetailsList, setVideoList] = useState(videodetails);
-  const [videoSmallList, setVideoSmallList] = useState(videoSmallListData);
 
-  const videoHandler = (id) => {
-    // return video object from details list where id matches
-    const video = VideoDetailsList.find((video) => {
-      if (video.id === id) {
-        return video;
-      }
-    });
+  const [videoSmallList, setVideoSmallList] = useState(null);
+  const [videoDetails, setvideoDetails] = useState(null);
 
-    setVideo(video);
+  const getVideoList = async () => {
+    const { data } = await axios.get(
+      `https://project-2-api.herokuapp.com/videos/?api_key=4cc34eef-8f4a-4587-ad97-0594e067e347`
+    );
+    setVideoSmallList(data);
   };
+
+  const getVideoDetails = async (id) => {
+    const { data } = await axios.get(
+      `https://project-2-api.herokuapp.com/videos/${id}?api_key=${key}`
+    );
+    setvideoDetails(data);
+  };
+
+  useEffect(() => {
+    try {
+      getVideoList();
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (videoSmallList) {
+      try {
+        getVideoDetails(videoSmallList[0].id);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }, [videoSmallList]);
 
   return (
     <>
-      <CurrentVideo currentvideo={video} />
+      <CurrentVideo videoImage={videoDetails} />
       <div className="home__wrapper">
         <div className="home__vid-desc-wrapper">
-          <VideoDescription currentdescription={video} />
+          <VideoDescription currentdescription={videoDetails} />
           <AddComment text={text} changeText={changeText} />
-          <Comments currentcomments={video} />
+          <Comments currentcomments={videoDetails} />
         </div>
         <VideoList
-          videoSmallList={videoSmallList}
-          videoHandler={videoHandler}
-          currentvideo={video}
+          videoSmallList={videoDetails}
+          videoHandler={videoDetails}
+          currentvideo={videoDetails}
         />
       </div>
     </>
