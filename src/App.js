6137f06bef198a9ba42c34b1926ad60252642070
BigBoiJonny <jonny98.0@outlook.com>
2023-03-07 14:37:17 +0000

    set up upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 // import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 
 import Home from "./pages/Home/Home";
-
+import Upload from "./pages/Upload/Upload";
 import "./styles/App.scss";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Home />
-    </>
+    <BrowserRouter>
+      <>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="upload" element={<Upload />} />
+        </Routes>
+      </>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import avatar from "../../assets/images/Mohan-muruge.jpg";
 import logo from "../../assets/logo/BrainFlix-logo.svg";
 
@@ -7,7 +9,9 @@ const Header = () => {
   return (
     <>
       <header className="header">
-        <img className="header__logo" src={logo} alt="logo" />
+        <Link to="/">
+          <img className="header__logo" src={logo} alt="logo" />
+        </Link>
         <div className="header__row">
           <input
             className="header__search-bar"
@@ -21,7 +25,9 @@ const Header = () => {
             alt="Mohan-Muruge"
           />
         </div>
-        <button className="header__upload-button">Upload</button>
+        <Link className="header__upload-button" to="/upload">
+          <h3 className="header__button-text">Upload</h3>
+        </Link>
         <img
           className="header__search-icon header__search-icon--2"
           src={avatar}
